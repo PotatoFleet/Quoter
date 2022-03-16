e638efa79915d8c3452410583ee7278d996ad3c6
PotatoFleet <supsrasri@gmail.com>
2022-03-16 18:02:18 +0530

    more cors testing

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(
   session({ resave: true, secret: "valorantvishal", saveUninitialized: true })
 );
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", api);
